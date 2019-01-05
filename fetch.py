f60abfeee60380ca81452352ad0cf0416045966e
Stephen Edwards <sventi555@gmail.com>
2019-01-05 14:00:47 -0500

    added dynamic entries for user login, term selection, and variable number of courses

--- fetch.py
+++ fetch.py
@@ -5,6 +5,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from getpass import getpass
+
+
+def find(field_type, field, wait):
+    if field_type == 'id':
+        return wait.until(EC.visibility_of_element_located((By.ID, field)))
+    elif field_type == 'xpath':
+        return wait.until(EC.visibility_of_element_located((By.XPATH, field)))
+    elif field_type == 'link_text':
+        return wait.until(EC.visibility_of_element_located((By.LINK_TEXT, field)))
+    elif field_type == 'name':
+        return wait.until(EC.visibility_of_element_located((By.NAME, field)))
+
 
 def main():
     options = Options()
@@ -18,44 +31,58 @@ def main():
     assert 'Quest' in driver.title
 
     print('going to login page')
-    sign_in = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Sign in')))
+    sign_in = find('link_text', 'Sign in', wait)
     sign_in.click()
 
     print('signing in')
-    username = wait.until(EC.visibility_of_element_located((By.ID, 'username')))
-    password = wait.until(EC.visibility_of_element_located((By.ID, 'password')))
-    login_button = wait.until(EC.visibility_of_element_located((By.NAME, '_eventId_proceed')))
+    username = find('id', 'username', wait)
+    password = find('id', 'password', wait)
+    login_button = find('name', '_eventId_proceed', wait)
 
     username.clear()
     username.send_keys(input('enter your username: '))
 
     password.clear()
-    password.send_keys(input('enter your password: '))
+    password.send_keys(getpass('enter your password: '))
 
     login_button.click()
 
     assert 'Student Center' in driver.title
 
     print('going to my academics')
-    frame = wait.until(EC.visibility_of_element_located((By.XPATH, '//iframe')))
+    frame = find('xpath', '//iframe', wait)
     driver.switch_to.frame(frame)
-    my_academics = wait.until(EC.visibility_of_element_located((By.ID, 'DERIVED_SSS_SCR_SSS_LINK_ANCHOR1')))
+    my_academics = find('id', 'DERIVED_SSS_SCR_SSS_LINK_ANCHOR1', wait)
     my_academics.click()
 
-    print('selecting term')
-    radio_input = wait.until(EC.visibility_of_element_located((By.ID, 'SSR_DUMMY_RECV1$sels$1$$0')))
+    term_table = find('xpath', "//table[@id='SSR_DUMMY_RECV1$scroll$0']/tbody", wait)
+    term_rows = term_table.find_elements_by_xpath('tr[position()>2]')
+    num_terms = len(term_rows)
+
+    print(f'Select the term you want to view (0...{num_terms - 1})')
+    for i in range(num_terms):
+        label = term_rows[i].find_element_by_id(f'TERM_CAR${i}').text
+        print(f'{i}: ' + label)
+
+    selected_term = input('Enter a valid option: ')
+
+    radio_input = find('id', f'SSR_DUMMY_RECV1$sels${selected_term}$$0', wait)
     radio_input.click()
 
-    cont_button = wait.until(EC.visibility_of_element_located((By.ID, 'UW_DRVD_SSS_SCT_SSR_PB_GO')))
+    cont_button = find('id', 'UW_DRVD_SSS_SCT_SSR_PB_GO', wait)
     cont_button.click()
 
+    grades_table = find('id', 'TERM_CLASSES$scroll$0', wait)
+    grade_rows = grades_table.find_elements_by_xpath('.//table/tbody/tr[position()>1]')
+    num_grades = len(grade_rows)
+
     print('\n')
-    for i in range(5):
+    for i in range(num_grades):
         label = wait.until(EC.visibility_of_element_located((By.ID, f'CLS_LINK$span${i}')))
         mark = wait.until(EC.visibility_of_element_located((By.ID, f'STDNT_ENRL_SSV1_CRSE_GRADE_OFF${i}')))
 
         print(label.text + ' - ' + mark.text)
-        print(' - ')
+        print('----------------------')
 
 
 if __name__ == "__main__":
